7e9fd95a8bbe284bebf90f58f64f8a4280d501c9
Niki Nikolaev <99727402+nikolaynikolaev777@users.noreply.github.com>
2022-05-29 12:01:05 +0200

    Add files via upload

---
+++ testingFile/webSiteOnProgress/muOnlineHelper/content/shield/skullshield.php
@@ -0,0 +1,219 @@
+<label class="popbox-BG" for="skullshield"></label>
+<div class="popbox-conn">
+	<label class="popbox-close" for="skullshield"></label>
+	<center><p>Skull Shield</p></center>
+	<div class="content">
+		<input id="stat-skullshield" type="checkbox" name="menu" />
+		<label class="sst" for="stat-skullshield">SHOW ITEM STAT</label>
+		<ul class="submenu">
+
+		<table class="tbconwidth">
+			<tbody>
+				<tr>
+					<td bgcolor="8083AF"><div align="center">Level</div></td>
+					<td colspan="2" bgcolor="EFEC50"><div align="center">Normal</div></td>
+					<td colspan="2" bgcolor="DF5326"><div align="center">Normal Requires</div></td>
+					<td colspan="2" bgcolor="A2DA3F"><div align="center">Excellent</div></td>
+					<td colspan="2" bgcolor="DF5326"><div align="center">Excellent Requires</div></td>
+				</tr>
+				<tr>
+					<td bgcolor="8083AF"></td>
+					<td bgcolor="D8C935"><div align="center">Min</div></td>
+					<td bgcolor="D8C935"><div align="center">Max</div></td>
+					<td bgcolor="B24D2C"><div align="center">Stenght</div></td>
+					<td bgcolor="B24D2C"><div align="center">Agility</div></td>
+					<td bgcolor="83BF32"><div align="center">Min</div></td>
+					<td bgcolor="83BF32"><div align="center">Max</div></td>
+					<td bgcolor="B24D2C"><div align="center">Strenght</div></td>
+					<td bgcolor="B24D2C"><div align="center">Agility</div></td>
+				</tr>
+  
+  
+  
+  
+  
+  
+  <tr>
+    <td><div align="center">0</div></td>
+    <td><div align="center">1</div></td>
+    <td><div align="center">6</div></td>
+    <td><div align="center">21</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">31</div></td>
+    <td><div align="center">36</div></td>
+    <td><div align="center">59</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">1</div></td>
+    <td><div align="center">4</div></td>
+    <td><div align="center">9</div></td>
+    <td><div align="center">26</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">34</div></td>
+    <td><div align="center">39</div></td>
+    <td><div align="center">63</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">2</div></td>
+    <td><div align="center">7</div></td>
+    <td><div align="center">12</div></td>
+    <td><div align="center">30</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">37</div></td>
+    <td><div align="center">42</div></td>
+    <td><div align="center">68</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">3</div></td>
+    <td><div align="center">10</div></td>
+    <td><div align="center">15</div></td>
+    <td><div align="center">35</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">40</div></td>
+    <td><div align="center">45</div></td>
+    <td><div align="center">72</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">4</div></td>
+    <td><div align="center">13</div></td>
+    <td><div align="center">18</div></td>
+    <td><div align="center">39</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">43</div></td>
+    <td><div align="center">48</div></td>
+    <td><div align="center">77</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">5</div></td>
+    <td><div align="center">16</div></td>
+    <td><div align="center">21</div></td>
+    <td><div align="center">44</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">46</div></td>
+    <td><div align="center">51</div></td>
+    <td><div align="center">81</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">6</div></td>
+    <td><div align="center">19</div></td>
+    <td><div align="center">24</div></td>
+    <td><div align="center">48</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">49</div></td>
+    <td><div align="center">54</div></td>
+    <td><div align="center">86</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">7</div></td>
+    <td><div align="center">22</div></td>
+    <td><div align="center">27</div></td>
+    <td><div align="center">53</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">52</div></td>
+    <td><div align="center">57</div></td>
+    <td><div align="center">90</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">8</div></td>
+    <td><div align="center">25</div></td>
+    <td><div align="center">30</div></td>
+    <td><div align="center">57</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">55</div></td>
+    <td><div align="center">60</div></td>
+    <td><div align="center">95</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">9</div></td>
+    <td><div align="center">28</div></td>
+    <td><div align="center">33</div></td>
+    <td><div align="center">62</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">58</div></td>
+    <td><div align="center">63</div></td>
+    <td><div align="center">99</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">10</div></td>
+    <td><div align="center">32</div></td>
+    <td><div align="center">37</div></td>
+    <td><div align="center">66</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">62</div></td>
+    <td><div align="center">67</div></td>
+    <td><div align="center">104</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">11</div></td>
+    <td><div align="center">37</div></td>
+    <td><div align="center">42</div></td>
+    <td><div align="center">71</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">67</div></td>
+    <td><div align="center">72</div></td>
+    <td><div align="center">108</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">12</div></td>
+    <td><div align="center">43</div></td>
+    <td><div align="center">48</div></td>
+    <td><div align="center">75</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">73</div></td>
+    <td><div align="center">78</div></td>
+    <td><div align="center">113</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">13</div></td>
+    <td><div align="center">50</div></td>
+    <td><div align="center">55</div></td>
+    <td><div align="center">80</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">80</div></td>
+    <td><div align="center">85</div></td>
+    <td><div align="center">117</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">14</div></td>
+    <td><div align="center">58</div></td>
+    <td><div align="center">63</div></td>
+    <td><div align="center">84</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">88</div></td>
+    <td><div align="center">93</div></td>
+    <td><div align="center">122</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+  <tr>
+    <td><div align="center">15</div></td>
+    <td><div align="center">67</div></td>
+    <td><div align="center">72</div></td>
+    <td><div align="center">89</div></td>
+    <td><div align="center">~</div></td>
+    <td><div align="center">97</div></td>
+    <td><div align="center">102</div></td>
+    <td><div align="center">126</div></td>
+    <td><div align="center">~</div></td>
+  </tr>
+</tbody></table>
+
+</ul>
+
+<center>
+	<h3>How can we find it</h3>
+	</div>
+</div>
