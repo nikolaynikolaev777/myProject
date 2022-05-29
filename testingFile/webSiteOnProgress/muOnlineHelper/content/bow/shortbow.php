1b918954844af7ef991bfb3d1c9dbb49c7960d92
Niki Nikolaev <99727402+nikolaynikolaev777@users.noreply.github.com>
2022-05-29 11:53:58 +0200

    Add files via upload

---
+++ testingFile/webSiteOnProgress/muOnlineHelper/content/bow/shortbow.php
@@ -0,0 +1,8 @@
+<label class="popbox-BG" for="shortbow"></label>
+<div class="popbox-conn">
+	<label class="popbox-close" for="shortbow"></label>
+	<center><p>Short Bow</p></center>
+	<div class="content">
+		
+	</div>
+</div>
