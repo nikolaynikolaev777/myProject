87d7b059ad2c9d8bb5e44e70e20a4b76cb72b363
Niki Nikolaev <99727402+nikolaynikolaev777@users.noreply.github.com>
2022-05-29 12:14:38 +0200

    Add files via upload

---
+++ testingFile/webSiteOnProgress/muOnlineHelper/core/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>MU-ONLINE HELPER</title>
+		<link rel="icon" type="../img/news/icon" href="../img/news/icon.png" />
+		<link rel="stylesheet" href="../core/css/style.css">
+		<link rel="stylesheet" href="../core/css/mobile.css">
+	</head>
+	<body>
+		<marquee behavior="scroll" direction="left">NEW UPDATE: ITEMS INFORMATION / CHAMPION INFORMATION ... See all update <a href="#"><u>here</u></a></marquee>
+		<video src="../core/css/img/1.mp4" autoplay loop playsinline muted></video>
+		<div class="headerlogo"><h1><center><span>M U</span><span class="light">H E L P E R</span> <span>ONLINE</span></center></h1></div>
+		<div class="headermobile"></div>
+		<div class="page">
+		<div class="magic-gladiator-banner"><img src="../core/css/img/magic-gladiator.png"></div>
+		<div class="dark-knight-banner"><img src="../core/css/img/dark-knight.png"></div>
+		<div class="row">
+				<div class="leftside">
+					<div class="title">MU-ONLINE HELPER</div>
+					<input id="check01" type="checkbox" name="menu" />
+					<label class="sst" for="check01">SEASON</label>
+					<ul class="submenu">
+						<li><a href="../season/version97.php">VERSION 97</a></li>
+						<li><a href="../season/version99.php">VERSION 99</a></li>
+						<li><a href="../season/season1.php">SEASON 1</a></li>
+						<li><a href="../season/season2.php">SEASON 2</a></li>
+						<li><a href="../season/season3.php">SEASON 3</a></li>
+						<li><a href="../season/season4.php">SEASON 4</a></li>
+						<li><a href="../season/season5.php">SEASON 5</a></li>
+						<li><a href="../season/season6.php">SEASON 6</a></li>
+						<li><a href="../season/season7.php">SEASON 7</a></li>
+						<li><a href="../season/season8.php">SEASON 8</a></li>
+						<li><a href="../season/season9.php">SEASON 9</a></li>
+						<li><a href="../season/season10.php">SEASON 10</a></li>
+						<li><a href="../season/season11.php">SEASON 11</a></li>
+						<li><a href="../season/season12.php">SEASON 12</a></li>
+						<li><a href="../season/season13.php">SEASON 13</a></li>
+						<li><a href="../season/season14.php">SEASON 14</a></li>
+						<li><a href="../season/season15.php">SEASON 15</a></li>
+						<li><a href="../season/season16.php">SEASON 16</a></li>
+					</ul>
+				</div>
+			
+			<div class="centerside">
+			<div id="menufloor1">
+			<div id="menufloor2">
+				<label for="show-menu" class="show-menu">M E N U</label>
+				<input type="checkbox" id="show-menu" role="button" />
+				<ul id="menu">
+					<li><a href="../Index.php">HOME</a></li>
+					<li><a href="../about.php">ABOUT US</a></li>
+					<li>
+						<a href="#">ITEMS</a>
+						<ul class="hidden">
+							<li><a href="../content/weapons.php">WEAPONS</a></li>
+							<li><a href="#">SET</a></li>
+							<li><a href="#">ANCIENT SET</a></li>
+							<li><a href="#">WING</a></li>
+							<li><a href="#">MORE ITEMS</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">CHARACTER</a>
+						<ul class="hidden">
+							<li><a href="../content/darkwizard.php">DARK WIZARD</a></li>
+							<li><a href="#">DARK KNIGHT</a></li>
+							<li><a href="#">ELF</a></li>
+							<li><a href="#">DARK LORD</a></li>
+							<li><a href="#">MAGIC GLADIATOR</a></li>
+							<li><a href="#">SUMMONER</a></li>
+							<li><a href="#">RAGE FIGHTER</a></li>
+							<li><a href="#">GROW LANCER</a></li>
+							<li><a href="#">RUNE WIZARD</a></li>
+							<li><a href="#">SLAYER</a></li>
+							<li><a href="#">GUN CRUSHER</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">MORE</a>
+						<ul class="hidden">
+							<li><a href="#">VIDEO</a></li>
+							<li><a href="#">GALERY</a></li>
+							<li><a href="#">COMMANDS</a></li>
+							<li><a href="#">PROBLEMS</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
