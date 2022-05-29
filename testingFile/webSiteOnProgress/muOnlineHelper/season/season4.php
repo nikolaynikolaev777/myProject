84fdd4293e4e19eace8cb6c8698b0507b9bfc48b
Niki Nikolaev <99727402+nikolaynikolaev777@users.noreply.github.com>
2022-05-29 12:15:58 +0200

    Add files via upload

---
+++ testingFile/webSiteOnProgress/muOnlineHelper/season/season4.php
@@ -0,0 +1,283 @@
+<?php include'../core/header.php'?>
+		
+			
+		<div class="title">MU-ONLINE SEASON 4</div>
+		
+		<center><h5>MU-ONLINE SEASON 4 have new map Rackleon (La Cleon) / mew Racleon Event / Socket Items / Socket System Sphere / PvP Duel Map</h5></center>
+		
+		<center>
+			<a href="../character/dw/dw.php">
+				<div class="imgbox">
+					<img src="../img/character/dw/dw.png" alt="Dark Wizard" width="100px">
+					<div class="imgboxtxt">Magic Gladiator</div>
+				</div>
+			</a>
+			<a href="../character/dk/dk.php">
+				<div class="imgbox">
+					<img src="../img/character/dk/dk.png" alt="Dark Knight" width="100px">
+					<div class="imgboxtxt">Dark Knight</div>
+				</div>
+			</a>
+			<a href="../character/elf/elf.php">
+				<div class="imgbox">
+					<img src="../img/character/elf/elf.png" alt="Elf" width="100px">
+					<div class="imgboxtxt">Elf</div>
+				</div>
+			</a>
+			<a href="../character/dl/dl.php">
+				<div class="imgbox">
+					<img src="../img/character/dl/dl.png" alt="Dark Lord" width="100px">
+					<div class="imgboxtxt">Dark Lord</div>
+				</div>
+			</a>
+			<a href="../character/mg/mg.php">
+				<div class="imgbox">
+					<img src="../img/character/mg/mg.png" alt="Magic Gladiator" width="100px">
+					<div class="imgboxtxt">Magic Gladiator</div>
+				</div>
+			</a>
+			<a href="../character/summoner/summoner.php">
+				<div class="imgbox">
+					<img src="../img/character/summoner/summoner.png" alt="Summoner" width="100px">
+					<div class="imgboxtxt">Summoner</div>
+				</div>
+			</a>
+		</center>
+		
+		<input id="maps97" type="checkbox" name="menu" />
+		<label class="sst" for="maps97">MU-ONLINE SEASON 4 ALL MAPS</label>
+		<ul class="submenu">
+			<center>
+				<table>
+					<tr>
+						<th style="text-align: left;">Map</th>
+						<th style="text-align: center;">Required Level</th>
+						<th style="text-align: right;">Required Zen</th>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Arena</td>
+						<td style="text-align: center;">200</td>
+						<td style="text-align: right;">30 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Lorencia</td>
+						<td style="text-align: center;">30</td>
+						<td style="text-align: right;">3 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Noria</td>
+						<td style="text-align: center;">30</td>
+						<td style="text-align: right;">3 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Elbeland</td>
+						<td style="text-align: center;">30</td>
+						<td style="text-align: right;">3 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Davias</td>
+						<td style="text-align: center;">50~80</td>
+						<td style="text-align: right;">8 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Dungeon</td>
+						<td style="text-align: center;">40~80</td>
+						<td style="text-align: right;">18 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Atlans</td>
+						<td style="text-align: center;">90~150</td>
+						<td style="text-align: right;">20 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Lost Tower</td>
+						<td style="text-align: center;">100~150</td>
+						<td style="text-align: right;">16 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Tarkan</td>
+						<td style="text-align: center;">150~200</td>
+						<td style="text-align: right;">25 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Icarus</td>
+						<td style="text-align: center;">250</td>
+						<td style="text-align: right;">27 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Kalima</td>
+						<td style="text-align: center;">50~350</td>
+						<td style="text-align: right;">Lost Map</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Aida</td>
+						<td style="text-align: center;">220~250</td>
+						<td style="text-align: right;">25 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Crywolf</td>
+						<td style="text-align: center;">300</td>
+						<td style="text-align: right;">30 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Barracks</td>
+						<td style="text-align: center;">370</td>
+						<td style="text-align: right;">35 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Refuge</td>
+						<td style="text-align: center;">380</td>
+						<td style="text-align: right;">37 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Santa Village</td>
+						<td style="text-align: center;">50</td>
+						<td style="text-align: right;">Santa Invitation</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Kanturo</td>
+						<td style="text-align: center;">200~300</td>
+						<td style="text-align: right;">30 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Kanturo Remain</td>
+						<td style="text-align: center;">350</td>
+						<td style="text-align: right;">40 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Swamp of Calmess</td>
+						<td style="text-align: center;">400</td>
+						<td style="text-align: right;">45 000</td>
+					</tr>
+					<tr style="background: #BF1626; color: #fff;">
+						<td style="text-align: left;">Racleon</td>
+						<td style="text-align: center;">350</td>
+						<td style="text-align: right;">50 000</td>
+					</tr>
+				</table>
+			</center>
+		</ul>
+		
+		<center><h5>Mu-Online Events</h5></center>
+		<center>
+			<table>
+				<tr>
+					<th style="text-align: left;">Event</th>
+					<th style="text-align: left;"></th>
+					<th style="text-align: center;">Information</th>
+					<th style="text-align: center;">Tutorial</th>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Blood Castle</td>
+					<td style="text-align: center;"><img src="icon/bc.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="bc-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="bc-example" type="checkbox" />
+						<div class="popbox"><?php include'event/bc-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="cstutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="cstutorial" type="checkbox" />
+						<div class="popbox"><?php include'cstutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Devil Square</td>
+					<td style="text-align: center;"><img src="icon/ds.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="ds-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="ds-example" type="checkbox" />
+						<div class="popbox"><?php include'event/ds-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="dstutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="dstutorial" type="checkbox" />
+						<div class="popbox"><?php include'dstutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Golden Invasion</td>
+					<td style="text-align: center;"><img src="icon/gi.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="gi-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="gi-example" type="checkbox" />
+						<div class="popbox"><?php include'event/gi-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="ginvtutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="ginvtutorial" type="checkbox" />
+						<div class="popbox"><?php include'ginvtutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Chaos Castle</td>
+					<td style="text-align: center;"><img src="icon/cc.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="cc-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="cc-example" type="checkbox" />
+						<div class="popbox"><?php include'event/cc-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="cctutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="cctutorial" type="checkbox" />
+						<div class="popbox"><?php include'cctutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Crywolf Forest</td>
+					<td style="text-align: center;"><img src="icon/cw.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="cw-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="cw-example" type="checkbox" />
+						<div class="popbox"><?php include'event/cw-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="cftutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="cftutorial" type="checkbox" />
+						<div class="popbox"><?php include'cftutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Kanturo</td>
+					<td style="text-align: center;"><img src="icon/knt.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="knt-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="knt-example" type="checkbox" />
+						<div class="popbox"><?php include'event/knt-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="ktutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="ktutorial" type="checkbox" />
+						<div class="popbox"><?php include'ktutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Castle Siege</td>
+					<td style="text-align: center;"><img src="icon/cs.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="cs-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="cs-example" type="checkbox" />
+						<div class="popbox"><?php include'event/cs-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="cstutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="cstutorial" type="checkbox" />
+						<div class="popbox"><?php include'cstutorial.php'?></div>
+					</td>
+				</tr>
+				<tr style="background: #BF1626; color: #fff;">
+					<td style="text-align: left;">Raklion Hatchery</td>
+					<td>
+						<label style="cursor:pointer;" for="rk-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="rk-example" type="checkbox" />
+						<div class="popbox"><?php include'event/rk-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="rtutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="rtutorial" type="checkbox" />
+						<div class="popbox"><?php include'rtutorial.php'?></div>
+					</td>
+				</tr>
+			</table>
+		</center>
+<?php include'../core/footer.php'?>
---
+++ testingFile/webSiteOnProgress/muOnlineHelper/season/version97.php
@@ -0,0 +1,193 @@
+<?php include'../core/header.php'?>
+		
+			
+		<div class="title">MU-ONLINE VERSION 97</div>
+		
+		<center><h5>MU Online (Korean: 뮤 온라인) is an Isometric medieval fantasy MMORPG, produced by Webzen, a Korean gaming company.</h5></center>
+		
+		<center>
+			<a href="../character/dw/dw.php">
+				<div class="imgbox">
+					<img src="../img/character/dw/dw.png" alt="Dark Wizard" width="100px">
+					<div class="imgboxtxt">Magic Gladiator</div>
+				</div>
+			</a>
+			<a href="../character/dk/dk.php">
+				<div class="imgbox">
+					<img src="../img/character/dk/dk.png" alt="Dark Knight" width="100px">
+					<div class="imgboxtxt">Dark Knight</div>
+				</div>
+			</a>
+			<a href="../character/elf/elf.php">
+				<div class="imgbox">
+					<img src="../img/character/elf/elf.png" alt="Elf" width="100px">
+					<div class="imgboxtxt">Elf</div>
+				</div>
+			</a>
+			<a href="../character/dl/dl.php">
+				<div class="imgbox">
+					<img src="../img/character/dl/dl.png" alt="Dark Lord" width="100px">
+					<div class="imgboxtxt">Dark Lord</div>
+				</div>
+			</a>
+		</center>
+		
+		<input id="maps97" type="checkbox" name="menu" />
+		<label class="sst" for="maps97">MU-ONLINE VERSION 97 ALL MAPS</label>
+		<ul class="submenu">
+			<center>
+				<table>
+					<tr>
+						<th style="text-align: left;">Map</th>
+						<th style="text-align: center;">Required Level</th>
+						<th style="text-align: right;">Required Zen</th>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Arena</td>
+						<td style="text-align: center;">200</td>
+						<td style="text-align: right;">30 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Lorencia</td>
+						<td style="text-align: center;">30</td>
+						<td style="text-align: right;">3 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Noria</td>
+						<td style="text-align: center;">30</td>
+						<td style="text-align: right;">3 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Davias</td>
+						<td style="text-align: center;">50~80</td>
+						<td style="text-align: right;">8 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Dungeon</td>
+						<td style="text-align: center;">40~80</td>
+						<td style="text-align: right;">18 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Atlans</td>
+						<td style="text-align: center;">90~150</td>
+						<td style="text-align: right;">20 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Lost Tower</td>
+						<td style="text-align: center;">100~150</td>
+						<td style="text-align: right;">16 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Tarkan</td>
+						<td style="text-align: center;">150~200</td>
+						<td style="text-align: right;">25 000</td>
+					</tr>
+					<tr>
+						<td style="text-align: left;">Icarus</td>
+						<td style="text-align: center;">250</td>
+						<td style="text-align: right;">27 000</td>
+					</tr>
+				</table>
+			</center>
+		</ul>
+		
+		<center><h5>Mu-Online Events</h5></center>
+		<center>
+			<table>
+				<tr>
+					<th style="text-align: left;">Event</th>
+					<th style="text-align: left;"></th>
+					<th style="text-align: center;">Information</th>
+					<th style="text-align: center;">Tutorial</th>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Blood Castle</td>
+					<td style="text-align: center;"><img src="icon/bc.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="bc-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="bc-example" type="checkbox" />
+						<div class="popbox"><?php include'event/bc-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="cstutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="cstutorial" type="checkbox" />
+						<div class="popbox"><?php include'cstutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Devil Square</td>
+					<td style="text-align: center;"><img src="icon/ds.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="ds-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="ds-example" type="checkbox" />
+						<div class="popbox"><?php include'event/ds-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="dstutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="dstutorial" type="checkbox" />
+						<div class="popbox"><?php include'dstutorial.php'?></div>
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align: left;">Golden Invasion</td>
+					<td style="text-align: center;"><img src="icon/gi.png"></td>
+					<td>
+						<label style="cursor:pointer;" for="gi-example"><div class="examplebtn">Example</div></label>
+						<input class="popbox-post" id="gi-example" type="checkbox" />
+						<div class="popbox"><?php include'event/gi-example.php'?></div>
+					</td>
+					<td>
+						<label style="cursor:pointer;" for="ginvtutorial"><div class="tutorialbtn">TUTORIAL</div></label>
+						<input class="popbox-post" id="ginvtutorial" type="checkbox" />
+						<div class="popbox"><?php include'ginvtutorial.php'?></div>
+					</td>
+				</tr>
+			</table>
+		</center>
+		<center>
+			<h4>Gameplay</h4>
+			<p>
+				MU Online was created in December 2001 by the Korean gaming company WEBZEN Inc. Like most MMORPGs, 
+				players have to create a character among nine different classes and to set their foot on the MU Continent. 
+				In order to gain experience and thus to level up, a players needs to fight monsters (mobs). 
+				MU is populated by a large variety of monsters, from simple ones like goblins and golems, 
+				to frightening ones such as the Gorgon, Kundun or Selupan. Each monster-type is unique, 
+				has different spawn points, and drops different items. Outside of a duel, PvP fighting is possible but discouraged. 
+				When one player attacks another, a self-defense system is activated, 
+				in which the attacked player may kill the aggressor within a time limit. 
+				Self-defense is also activated when a player attacks an Elf's summoned creature.
+				 Outlaws have disadvantages during game play, depending on their Outlaw level.
+			</p>
+			<h4>Character classes</h4>
+			<p>
+				At the start of the game the players can choose between three different character classes - 
+				Dark Wizard, Dark Knight and Fairy Elf. Players can unlock two classes if they reach certain 
+				level with one character: Magic Gladiator, and Dark Lord. Also, character cards can be purchased 
+				in order to unlock more character classes. Each class has its specific powers and items. As characters 
+				level up and complete quests, they can transform themselves into stronger classes. Each progressive
+				class change grants, gain access to new spells, skills,
+				weapons and wings as well as an associated change in appearance.
+			</p>
+			<h5>Spells and Skills</h5>
+			<p>
+				Like most MMO games, characters in MU can use many different kinds of magic and special abilities.
+				Each character has its own set of spells and some weapons may be enchanted in order to provide the character with a specific spell. 
+				Casting a spell costs MP (mana, or magic point) and sometimes AG or stamina.
+			</p>
+			<h5>Communications</h5>
+			<p>
+				Mu Online has a built-in chat system that allows all visible players to easily communicate via text. Users can add custom players to 
+				their Friends List in-order to make it easy to keep in contact with one-another. Other methods of chat were available as well, such as Guild chat and Party chat.
+			</p>
+			<h5>Cooperative Play</h5>
+			<p>
+				Players can choose to team up with other players in a "party", in which players cooperate against enemy monsters to share exp and loot. 
+				Early seasons of the game encouraged party play by rewarding the player with the highest level more experience points for helping lower 
+				level players. Players could not see the level of other players in the party but because spending character points on vitality was highly 
+				discouraged, players could guess quite accurately the level of other players by seeing their health bars. Some builds were made for parties 
+				only like the part elf, who focused on filling up the energy stat, being protected by a summoned mob, and buffing party members.
+			</p>
+			<h4>The information is taken from <u><a href="https://en.wikipedia.org/wiki/Mu_Online">Wikipedia</a></u>
+		</center>
+		
+<?php include'../core/footer.php'?>
