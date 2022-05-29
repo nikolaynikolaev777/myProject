271d1407e43c6a74054571d4bcab75e53afc27b0
Niki Nikolaev <99727402+nikolaynikolaev777@users.noreply.github.com>
2022-05-29 12:16:34 +0200

    Add files via upload

---
+++ testingFile/webSiteOnProgress/muOnlineHelper/about.php
@@ -0,0 +1,15 @@
+<?php include'core/header.php'?>
+			
+		<div class="title" id="borderimg">ABOUT US</div>
+		<center>
+			<p>
+				Mu-Online Helper is a site that primarily aims to facilitate new players and teach them how to play this game.<br>
+				There is also a lot of information that can be useful for both new and old players.<br>
+				On web-site, have detailed information about each items of the game.<br>
+				Also has a detailed description of each hero, how to pass a quest and how to develop.
+			</p><hr>
+			<h4>ADMINISTRATOR EMAIL</h4>
+			<h5><b>pulsenikolaev@gmail.com</b></h5>
+		</center>
+		
+<?php include'core/footer.php'?>
---
+++ testingFile/webSiteOnProgress/muOnlineHelper/index.php
@@ -0,0 +1,95 @@
+<?php include'core/header.php'?>
+			
+		<center><p>The <i>Mu-Online Helper</i> site is entirely designed to help you quickly and easily find the related information you are looking for Mu-Online.<br>
+		The information on the site is constantly updated.</p></center>
+		
+		<div class="title">SEASON 16: Gun Crusher is here.</div>
+		<center>September 18 2020</center>
+		<div class="contbox">
+			<p>
+				WEBZEN announced the release of Season 16, the latest update to its classic MMORPG MU Online.<br><br>
+
+				-=-=-=-=-
+			</p>
+			<center><a href="season/season16.php"><div class="newsbtn">ALL ABOUT SEASON 16</div></a></center>
+		</div>
+		<img src="img/news/s16news.png">
+		
+		<div class="title">SEASON 15: Slayer is here.</div>
+		<center>December 20 2019</center>
+		<div class="contbox">
+			<p>
+				WEBZEN announced the release of Season 15, the latest update to its classic MMORPG MU Online.<br><br>
+
+				The Season 15 content update includes a new map ‘Abyss of Atlans’, 6th Grade Mastery Armor Item, and 5th Blue Eye Weapon along with an increased level cap up to level 1,100.<br><br>
+
+				Abys of Atlans is a new area available to enter for characters whose levels are higher than 1000.<br><br>
+				
+				In this new area, new monsters including Bahamut of Abyss, Vepar of Abyss, Medium Bahamut of Abyss, Silver Valkyrie of Abyss, Great Bahamut of Abyss, Lizard King of Abyss, (Elite) Great Bahamut of Abyss, (Elite) Lizard King of Abyss.
+			</p>
+			<center><a href="season/season15.php"><div class="newsbtn">ALL ABOUT SEASON 15</div></a></center>
+		</div>
+		<img src="img/news/s15news.png">
+		
+		<div class="title">MU ORIGIN 2</div>
+		<center>May 20 2019</center>
+		<div class="contbox">
+			<p>
+				It doesn’t get much bigger than MU Origin in the mobile MMORPG world. This acclaimed spinoff of Webzen’s MU Online has been dominating the Asian charts for the last couple of years, so the announcement earlier this year that MU Origin 2 is on the way was a big deal.<br><br>
+
+				And now we have the release date: May 28th, which is less than two weeks from now.<br><br>
+
+				MU Origin 2 will take the long-established MU universe and update it in all the important ways, with souped-up visuals, richer gameplay, and a more expansive world.<br><br>
+
+				After choosing your class – Dark Knight, Dark Wizard, or Elf – you’ll embark on an epic journey, conquering dungeons, completing quests, progressing through the story campaign, and taking on human opponents in a variety of modes.
+			</p>
+			<center><a href="http://muorigin2.webzen.com/en" target="_blank"><div class="newsbtn">OFFICIAL WEBSITE</div></a></center>
+		</div>
+		<img src="img/news/muo2news.jpg">
+		
+		<div class="title">SEASON 14: Rune Wizard is here.</div>
+		<center>Marth 09, 2019</center>
+		<div class="contbox">
+			<p>
+				WEBZEN announced the release of Season 14, the latest update to its classic MMORPG MU Online.<br><br>
+
+				The Season 14 content update includes a new map ‘Cubera Mines’, 5th Grade Mastery Armor Item, and 4th Grade Soul Weapon along with an increased level cap up to level 1000.<br><br>
+
+				Cubera Mines is a new area available to enter for characters whose levels are higher than 850.<br><br>
+
+				Cubera Mines was the first mine ever developed in the land of MU, but was soon closed and abandoned because the mine was infested with monsters. In this new area, new monsters including Mine Digger, Mine Carrier, Mine Driller, Mine Porter, Dead Digger, Dead Shoveler and Dead Porter await all MU Players.
+			</p>
+			<center><a href="season/season14.php"><div class="newsbtn">ALL ABOUT SEASON 14</div></a></center>
+		</div>
+		<img src="img/news/s14news.jpg">
+		
+		<div class="title">SEASON 13: Grow Lancer is here.</div>
+		<center>August 21, 2018</center>
+		<div class="contbox">
+			<p>
+				Webzen announced this week that Mu Online Season 13 Part 1 is set to launch on August 21, 2018 and will introduce a new map called 'Swamp of Darkness', new mastery weapons, an increased level cap to 920, and much more. The new Swamp of Darkness map is available to those level 800 or higher and introduces new powerful monsters for players to defeat. As with any big update that includes a new level cap - expect new gear and item drops.<br><br>
+
+				To celebrate the upcoming update, Webzen is hosting events in-game between now and August 21st with new events set to be announced on August 21. Despite Mu Online's age (launched in 2004), the game remains incredibly popular.
+			</p>
+			<center><a href="season/season13.php"><div class="newsbtn">ALL ABOUT SEASON 13</div></a></center>
+		</div>
+		<img src="img/news/s13news.jpg">
+		
+		
+		<div class="title">MU LEGEND</div>
+		<center>October 25, 2016</center>
+		<div class="contbox">
+			<p>
+				MU Legend is the official follow-up to Mu Online - one of the first 3D MMORPGs in South Korea. Like its predecessor, MU Legend is developed by the Korean gaming company Webzen Games.<br><br>
+
+				The title has been under development for quite some time - first rumors about it appeared in 2004 and its development was held back until 2009.<br><br>
+
+				MU2's debut trailer was showcased at the G-Star 2011 Expo in Busan on November 10, 2011. The game is based on Unreal Engine 3 and shares familiar features with Mu Online as distinctive UI and controls. On September 23, 2015 official Korean site was updated with teaser page.<br><br>
+
+				MU 2 has been renamed to MU Legend. On October 25, 2016 MU Legend starts its global release with a first closed beta test.
+			</p>
+			<center><a href="http://mulegend.webzen.com/en" target="_blank"><div class="newsbtn">OFFICIAL WEBSITE</div></a></center>
+		</div>
+		<img src="img/news/mulegend.jpg">
+		
+<?php include'core/footer.php'?>
