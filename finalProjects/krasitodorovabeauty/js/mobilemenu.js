8653bc44304f1ec7e5d744a26e988a370d7e7be2
Niki Nikolaev <99727402+nikolaynikolaev777@users.noreply.github.com>
2022-05-29 11:28:08 +0200

    Add files via upload

---
+++ finalProjects/krasitodorovabeauty/js/mobilemenu.js
@@ -0,0 +1,7 @@
+function mobilemenuOn() {
+		document.getElementById("mobileMenu").style.width = "100%";
+	}
+
+	function mobilemenuOff() {
+		document.getElementById("mobileMenu").style.width = "0%";
+	}
